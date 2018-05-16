207a7a596c99f9eae837b8242691509d44f9a339
wangrui <wr190853093@163.com>
2018-05-16 22:56:14 +0800

    设计完成，开始写models

--- author_manage/models.py
+++ author_manage/models.py
@@ -1,8 +1,20 @@
+#coding:utf-8
+
 from django.db import models
 
-# Create your models here.
-class organization(models.Model):
-    pass
 
-class user(models.Model):
-    pass
+class Organization(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    parent = models.IntegerField(null=True)
+    is_del = models.SmallIntegerField(choices=((0, u'已删除'), (1, u'未删除')), null=True)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=15)
+    username = models.CharField(max_length=20, unique=True, null=False)
+    email = models.EmailField()
+    org = models.ForeignKey(Organization)
+    role = models.SmallIntegerField(choices=((0, u'产品经理'), (1, u'开发'), (2, u'测试'),(3, u'运维')), null=False)
+    status = models.SmallIntegerField(choices=((0, u'离职'), (1, u'在职')), null=False)
+    is_del = models.SmallIntegerField(choices=((0, u'已删除'), (1, u'未删除')), null=True)
+    password = models.CharField(max_length=128, null=False)
--- project_manage/models.py
+++ project_manage/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+#coding:utf-8
 
-# Create your models here.
+from django.db import models
+from author_manage.models import *
+
+class Project(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    status = models.SmallIntegerField(choices=((0, u'禁用'), (1, u'启用')), null=False)
+    team = models.ManyToManyField(User)
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    parent = models.IntegerField(null=True)
+    project = models.ForeignKey(Project)
+    is_del = models.SmallIntegerField(choices=((0, u'已删除'), (1, u'未删除')), null=True)
+
+
+class Environment(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    host = models.CharField(max_length=20, null=False)
+    status = models.SmallIntegerField(choices=((0, u'禁用'), (1, u'启用')), null=False)
+    project = models.ForeignKey(Project)
+    is_del = models.SmallIntegerField(choices=((0, u'已删除'), (1, u'未删除')), null=True)
+    type = models.SmallIntegerField(choices=((0, u'开发环境'), (1, u'测试环境'), (2, u'准生产环境'),(3, u'生产环境')), null=False)
+
+
+class Api(models.Model):
+    pass
+
